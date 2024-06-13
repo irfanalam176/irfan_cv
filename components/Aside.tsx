4b9c7d68856e1ac54e1304966a8073f7eae55806
irfanalam176 <irfancs176@gmail.com>
2024-06-13 17:01:12 +0500

    first commit

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -10,7 +10,8 @@ function Aside() {
             <span>FullStack Developer</span>
             <div className="socialMedia">
                 <Link href={"https://www.fiverr.com/imweb_dev?source=gig_cards&referrer_gig_slug=develop-landing-page-using-tailwind-css-and-bootstrap&ref_ctx_id=e3230ad9f08a4a9ebaf996b225a0e44e&imp_id=d65e0024-c583-4e2f-bd8e-9297f886a3a6"} target='_blank'><Image src={"/assets/img/fiverr.svg"} alt='' width={30} height={30} /></Link>
-                <Link href={"https://www.linkedin.com/in/irfan-alam-96189922a/"} target='_blank'><Image src={"/assets/img/linkdIn.svg"} alt='' width={16} height={15} /></Link>
+                <Link href={"https://www.linkedin.com/in/irfan-alam-96189922a/"} target='_blank'>
+                <Image src={"/assets/img/linkedin.svg"} alt='' width={16} height={15} /></Link>
                 <Link href={"https://x.com/imirfan176"} target='_blank'><Image src={"/assets/img/twitter.svg"} alt='' width={17} height={14} /></Link>
                 <Link href={"https://github.com/irfanalam176"} target='_blank'><Image src={"/assets/img/github.svg"} alt='' width={17} height={16} /></Link>
             </div>
